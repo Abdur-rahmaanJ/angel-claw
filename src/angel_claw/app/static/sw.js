382491d1e97d11f86a7918237b108423572dc0c6
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2026-04-04 10:11:52 +0000

    feat: add PWA support and persistent login with custom login UI

---
+++ src/angel_claw/app/static/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'angel-claw-v1';
+const ASSETS = [
+  '/',
+  '/static/shopyo.png',
+  'https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

b80c012afd2d433651b72e104c7fed819cfc5d0b
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2026-04-05 11:34:06 +0000

    fix: icon

--- src/angel_claw/app/static/sw.js
+++ src/angel_claw/app/static/sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'angel-claw-v1';
+const CACHE_NAME = 'angel-claw-v2';
 const ASSETS = [
   '/',
-  '/static/shopyo.png',
+  '/static/wing.png',
   'https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
